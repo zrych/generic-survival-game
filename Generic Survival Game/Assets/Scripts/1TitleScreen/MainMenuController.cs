321fc2efa83e69e17fe4012914def85c669669a0
JustinePaulDaileg <0324-0494@lspu.edu.ph>
2025-12-08 16:53:13 +0800

    Added Features Main Menu

--- Generic Survival Game/Assets/Scripts/1TitleScreen/MainMenuController.cs
+++ Generic Survival Game/Assets/Scripts/1TitleScreen/MainMenuController.cs
@@ -1,10 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
 
 public class MainMenuController : MonoBehaviour
 {
+
+    public AudioMixer audioMixer;
+    public Slider musicSlider;
+    public Slider sfxSlider;
+
+
+    private void Start()
+    {
+        LoadVolume();
+        MusicManager.Instance.PlayMusic("MainMenu");
+    }
+
     public void PlayButtonClicked()
     {
         SceneManager.LoadScene("TWorldSelect");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void UpdateMusicVolume(float volume)
+    {
+        audioMixer.SetFloat("MusicVolume", volume);
+    }
+
+    public void UpdateSoundVolume(float volume)
+    {
+        audioMixer.SetFloat("SFXVolume", volume);
+    }
+
+    public void SaveVolume()
+    {
+        audioMixer.GetFloat("MusicVolume", out float musicVolume);
+        PlayerPrefs.SetFloat("MusicVolume", musicVolume);
+
+        audioMixer.GetFloat("SFXVolume", out float sfxVolume);
+        PlayerPrefs.SetFloat("SFXVolume", sfxVolume);
+    }
+
+    public void LoadVolume()
+    {
+        musicSlider.value = PlayerPrefs.GetFloat("MusicVolume");
+        sfxSlider.value = PlayerPrefs.GetFloat("SFXVolume");
     }
 }
